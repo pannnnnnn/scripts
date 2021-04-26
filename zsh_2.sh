ea5178958b0ca1a330d7b83c169bc051079adc78
pannnnnnn <62750189+pannnnnnn@users.noreply.github.com>
2021-04-26 21:56:51 +0900

    Create zsh_2.sh

---
+++ zsh_2.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sed -i 's/robbyrussell/agnoster/' .zshrc
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' .zshrc
+echo "RPROMPT='%t'" >> ~/.oh-my-zsh/themes/agnoster.zsh-theme
