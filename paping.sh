a112b209eb4eac3ee5c7f161ec79e5d62ed2dbaa
pannnnnnn <62750189+pannnnnnn@users.noreply.github.com>
2021-04-26 22:02:12 +0900

    Create paping.sh

---
+++ paping.sh
@@ -0,0 +1,5 @@
+wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/paping/paping_1.5.5_x86-64_linux.tar.gz
+tar xf paping_1.5.5_x86-64_linux.tar.gz
+rm paping_1.5.5_x86-64_linux.tar.gz
+chmod +x paping
+mv paping /usr/local/bin
