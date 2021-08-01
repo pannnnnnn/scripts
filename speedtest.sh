28151b566dc44a7caa34ad2e07b101a6b5a1e9fd
pannnnnnn <62750189+pannnnnnn@users.noreply.github.com>
2021-08-01 15:28:17 +0900

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -1,4 +1,2 @@
-wget "https://bintray.com/ookla/download/download_file?file_path=ookla-speedtest-1.0.0-x86_64-linux.tgz" -O speedtest.tgz
-tar -xf speedtest.tgz
-rm speedtest.tgz speedtest.5 speedtest.md
-mv speedtest /usr/local/bin
+curl -s https://install.speedtest.net/app/cli/install.deb.sh | bash
+apt install speedtest
